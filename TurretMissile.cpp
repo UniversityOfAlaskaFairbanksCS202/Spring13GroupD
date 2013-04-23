7d075275c58cc0a7012741874eefb5d95f8eeb5a
sjgeorge2 <sjgeorge34@yahoo.com>
2013-04-23 12:15:31 -0700

    Update TurretMissile.cpp

--- TurretMissile.cpp
+++ TurretMissile.cpp
@@ -11,39 +11,34 @@ TurretMissile::TurretMissile(Vec2f cursorLoc, Vec2f rect)
 {
   _health = true; 
 	_begLocation = Vec2f ( rect.x + 2.5, rect.y + 2.5);
-	_location = begLocation;
+	_mLocation = begLocation;
 	
 	_velocity = 2.0f;
 	_radius = 3.0f;
 	
-	_mPath = _dirToCursorLoc - _location;
+	_mPath = _cursorLoc - _mLocation;
 }
 
 // mouseMove + mouseDrag allow the turret to follow cursor //
 // gets mouse location //
 void Turret::mouseMove(MouseEvent event) 
 {
-		_dirToCursorLoc = event.getPos();
+		_cursorLoc = event.getPos();
 }
 
-// as you move the cursor, alerts mouseMove to get position //
-void Turret::mouseDrag(MouseEvent event) 
-{
-		mouseMove( event );	
-}
 
 // on left click, FIRE MISSILE //
 void TurretMissile::mouseDown( event ) 
 {
 	if ( event.isLeft()) {
-		TurretMissile(_mLoc, _rect);
+		TurretMissile(_mLocation, _rect);
 		}
 }
 
 // moves the missile, checks for collision //	
 void TurretMissile::update()
 {
-		_location += _dirToCursor * _velocity;
+		_mlocation += _cursorLoc * _velocity;
 	if(collisionDetection())
 	{
 		_radius = 100.0;
@@ -54,17 +49,17 @@ void TurretMissile::update()
 // draws missile path, if a hit, explosion!! //
 void TurretMissile::draw()
 {
-	gl::drawLine(_begLocation, _location);
+	gl::drawLine(_begLocation, _mLocation);
 	if (!_health)
 	{
-		gl::drawSolidCircle (_location, _radius);
+		gl::drawSolidCircle (_mLocation, _radius);
 	}
 }
 
 // Collision Detection //
 bool TurretMissile::collisionDetection() 
 {
-	if (_location == Missile._location) {
+	if (_mLocation == Missile._location) {
 		return true;
 	}
 	return false;
