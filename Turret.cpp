b11741e38cca804ec9b5c7abbba875d0935b3efa
sjgeorge2 <sjgeorge34@yahoo.com>
2013-04-23 15:56:59 -0700

    Update Turret.cpp

--- Turret.cpp
+++ Turret.cpp
@@ -1,14 +1,1 @@
-#include "Turret.h"
-#include "cinder/app/AppBasic.h"
 
-using namespace ci;
-using namespace ci::app;
-// draws base, cannon //
-void Turret::draw()
-{
-gl::drawOpenCircle(275, 400)
-
-// top side points have their own postion as well as bottom two //
-// 
-gl::drawOpenCircle(_startPosition.x, _startPosition.y, 10);
-}
