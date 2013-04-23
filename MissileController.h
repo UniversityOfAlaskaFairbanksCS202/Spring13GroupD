fdf2c73fbe63e48335cb79b674b8269ab6e70148
sjgeorge2 <sjgeorge34@yahoo.com>
2013-04-23 12:35:53 -0700

    Update MissileController.h

--- MissileController.h
+++ MissileController.h
@@ -1,8 +1,10 @@
 #ifndef MISSILE_H_INCLUDED
 #define MISSILE_H_INCLUDED
 
+
 #include "cinder/Channel.h"
 #include "Missile.h"
+#include "TurretMissile.h"
 #include <list>
 
 
@@ -15,8 +17,11 @@ public:
 	void draw();
 	void addMissiles(int);
 	void removeMissiles(int);
+	void addTurretMissile (int);
+	void removeTurretMissile (int);
 
 	std::list <Missile> _Missile;
+	std::list <TurretMissile> _TurretMissile;
 };
 
 
