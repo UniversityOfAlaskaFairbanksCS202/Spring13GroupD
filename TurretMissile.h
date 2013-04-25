0a1bb4fe8dee958a8528218b5adff34e2bb79c23
DrHoodrich <natehelms_8@gmx.com>
2013-04-25 14:07:25 -0700

    Update TurretMissile.h

--- TurretMissile.h
+++ TurretMissile.h
@@ -1,22 +1,34 @@
+#pragma once
 #ifndef TURRETMISSILE_H
 #define TURRETMISSILE_H
+
+
+#include "cinder/app/AppBasic.h"
+#include "cinder/gl/gl.h"
+#include "cinder/ImageIo.h"
+#include "cinder/gl/Texture.h"
+
 class TurretMissile
 {
 public:
-  	ci::Vec2f _mLocation;
-	ci::Vec2f _cursorLoc;
-	float _velocity;
-	float _radius;
-	bool _health;
-	
-	ci::Vec2f _missileStartHolder;
+ci::Vec2f _mLocation;
+ci::Vec2f _cursorLoc;
+float _velocity;
+float _radius;
+bool _health;
+int m;
 
-	TurretMissile(Vec2f, Vec2f);
-	
-	void update();
-	void draw();
-	bool collisionDetection();
+
+TurretMissile(ci::Vec2f, ci::Vec2f);
+
+void update();
+void draw();
+bool collisionDetection();
+void removeMissile(int);
+TurretMissile();
 
 };
+
+
 
 #endif
