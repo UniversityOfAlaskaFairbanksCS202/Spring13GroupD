ebb7f7d6483ac4205195184d3635a3f841577d09
DrHoodrich <natehelms_8@gmx.com>
2013-04-30 12:58:24 -0700

    Update Resources.h

--- CinderProjectTESTING/include/Resources.h
+++ CinderProjectTESTING/include/Resources.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "cinder/CinderResources.h"
 
-//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
+//#define RES_MY_RES CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
 
-#define RES_BUILDING          CINDER_RESOURCE (../resources/ , building.png, 128, IMAGE) // building image 
-#define RES_BACKGROUND          CINDER_RESOURCE (../resources/ , MC.BG.Placeholder.png, 129, IMAGE) //background image 
-
-
+#define RES_BUILDING1 CINDER_RESOURCE    (../resources/ , building1.png, 128, IMAGE) // building image
+#define RES_BACKGROUND CINDER_RESOURCE  (../resources/ , MC.BG.Placeholder.png, 129, IMAGE) //background image 
+#define RES_BUILDING2 CINDER_RESOURCE   (../resources/ , building2.png, 130, IMAGE) //building image after 1 hit
+#define RES_BUILDING3 CINDER_RESOURCE   (../resources/ , building3.png, 131, IMAGE) //building image after 2 hit
+#define RES_BUILDING4 CINDER_RESOURCE   (../resources/ , building4.png, 132, IMAGE) //building image after 3 hit
+#define RES_FRONTPAGE CINDER_RESOURCE (../resources/ , frontpage.png, 133, IMAGE) //Start Screen
