569152dc1e63b902d649cc8325ab1af9e96fb54c
DrHoodrich <natehelms_8@gmx.com>
2013-05-01 16:54:42 -0700

    Update ScoringEngine.h

--- ScoringEngine.h
+++ ScoringEngine.h
@@ -1,26 +1,30 @@
 #ifndef SCORING_ENGINE_H_INCLUDED
 #define SCORING_ENGINE_H_INCLUDED
 
+#include <iomanip>
+#include <locale>
+#include <sstream>
+#include <string>
 
 class ScoringEngine
 {
-
 public:
   ScoringEngine();
-	ScoringEngine( int );
-	void updateScore ();
-	//void setVelocity(int);
-	void updateVelocity();
-	float getVelocity();
-	void updateLevel();
-	int getMissiles();
+ScoringEngine( int );
+void updateScore ();
+//void setVelocity(int);
+void updateVelocity();
+float getVelocity();
+void updateLevel();
+int getMissiles();
 
-	static int _level;
-	static int _score;
-	static float _sVelocity;
-	static int _difficulty;
-	static int _missiles;
-	
+static int _level;
+static int _score;
+static float _sVelocity;
+static int _difficulty;
+static int _missiles;
+std::ostringstream convert;
+std::string endingscore;
 };
 
 
