5752bb7f784b5f0e80510efe79f8deaac6264aad
DrHoodrich <natehelms_8@gmx.com>
2013-05-01 16:54:11 -0700

    Update ScoringEngine.cpp

--- ScoringEngine.cpp
+++ ScoringEngine.cpp
@@ -6,104 +6,106 @@ int ScoringEngine::_difficulty=0;
 float ScoringEngine::_sVelocity=0;
 int ScoringEngine::_missiles=0;
 
-
 ScoringEngine::ScoringEngine(){}
 
 /*ScoringEngine::ScoringEngine(int choice)
 {
-  _level = choice;
+_level = choice;
 }*/
 
 
 int ScoringEngine::getMissiles()
 {
-	return _missiles;
+return _missiles;
 }
 
 ScoringEngine::ScoringEngine(int choice)
 {
-	_level = choice;
-	switch (choice)
-	{
-	case 1: { _sVelocity = 1.0f;
-			    _difficulty = 1;
+_level = choice;
+
+		switch (choice)
+		{
+			case 1: 
+			{ 
+				_sVelocity = 1.0f;
+				_difficulty = 1;
 				_missiles = 10;
 				break;
-			  }
-			  
-			  
+			}
 
-	case 2: {_sVelocity = 1.3f;
-			   _difficulty = 2;
-			   _missiles = 15;
-			   break;
-			  }
 
-			  
 
-	case 3:   { _sVelocity = 1.6f;
-			    _difficulty = 3;
+			case 2: 
+			{
+				_sVelocity = 1.3f;
+				_difficulty = 2;
+				_missiles = 15;
+				break;
+			}
+
+
+
+			case 3: 
+			{ 
+				_sVelocity = 1.6f;
+				_difficulty = 3;
 				_missiles = 20;
-			  break;
-			  }
+				break;
+			}
 
-			  
 
-	case 4: { _sVelocity = 8.0f;
-			    _difficulty = 4;
+
+			case 4: 
+			{ 
+				_sVelocity = 8.0f;
+				_difficulty = 4;
 				_missiles = 78;
-			  break;
-			 }
-	}
+			break;
+			}
+	} 
 }
 
 
 
 float ScoringEngine::getVelocity()
 {
-	return _sVelocity;
+return _sVelocity;
 }
 
 
 void ScoringEngine::updateScore()
 {
-	_score += 50;
+_score += 50;
+convert<<_score;
+endingscore = convert.str();
 }
 
 
 void ScoringEngine::updateLevel()
 {
-	++_level;
-	updateVelocity();
+++_level;
+updateVelocity();
 }
 
 
 void ScoringEngine::updateVelocity()
 {
-	switch(_difficulty)
-	{
-	case '1': { _sVelocity += 0.1f;
-				break;
-			  }
-
-	case '2': { _sVelocity += 0.3f;
-				break;
-			  }
-
-	case '3': { _sVelocity += 0.6f;
-				break;
-			  }
-			  
-	case '4': { _sVelocity += 5.0f;
-				break;
-			  }
-	}
+switch(_difficulty)
+{
+case '1': { _sVelocity += 0.1f;
+break;
 }
 
+case '2': { _sVelocity += 0.3f;
+break;
+}
 
+case '3': { _sVelocity += 0.6f;
+break;
+}
 
-
-
-
-
-
+case '4': { _sVelocity += 5.0f;
+break;
+}
+}
+}
