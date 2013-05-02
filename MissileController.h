62dd2b15b471a5ebd6d858d84a6835e8bafb7445
DrHoodrich <natehelms_8@gmx.com>
2013-05-02 12:21:03 -0700

    Commented Politely

--- MissileController.h
+++ MissileController.h
@@ -12,18 +12,18 @@ using namespace ci;
 class MissileController
 {
 public:
-    MissileController();
-	void update();
-	void draw();
-	void addMissiles(int);
-	void removeMissiles(int);
-	void addTurretMissile (int, Vec2f, Vec2f);
-	void removeTurretMissile (int);
+	MissileController(); //Default ctor
+	void update(); //Checks on the missiles status whenever called throughout the game.
+	void draw(); //Draws the missiles and explosions
+	void addMissiles(int); // Adds Missiles
+	void removeMissiles(int); //Removes Missiles When destroyed
+	void addTurretMissile (int, Vec2f, Vec2f); //Add turret Missiles to the screen
+	void removeTurretMissile (int); //Removes turret missiles
     
-	std::list <Missile> _Missile;
-	std::list <TurretMissile> _TurretMissile;
+	std::list <Missile> _Missile; //Creates the list of Missiles
+	std::list <TurretMissile> _TurretMissile; //Creates the list of turret Missiles
     
-   bool collisionDetection(int radius, Vec2f turretMissileLoc, Vec2f missileLoc);
+   bool collisionDetection(int radius, Vec2f turretMissileLoc, Vec2f missileLoc); //Collision detection for Missiles and Turret Missiles
 };
 
 
