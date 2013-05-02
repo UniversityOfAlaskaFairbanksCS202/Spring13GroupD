11b1f47a09b758227c34ff9d6a836211b020f375
lstrunk2 <lstrunk2@alaska.edu>
2013-05-01 23:26:45 -0700

    added comments

--- Missile.cpp
+++ Missile.cpp
@@ -10,64 +10,62 @@
 
 using namespace ci;
 
-static Vec2f _Building1 = Vec2f( 50.0f, 487.0f );
-static Vec2f _Building2 = Vec2f( 200.0f, 487.0f );
-static Vec2f _Building3 = Vec2f( 510.0f, 487.0f );
-static Vec2f _Building4 = Vec2f( 660.0f, 487.0f );
-static Vec2f _Building[4] = {_Building1, _Building2, _Building3, _Building4};
-static Vec2f centerCorrection = Vec2f (40.0f, 40.0f);
-
-static MissileController _MissileController;
+static Vec2f _Building1 = Vec2f( 50.0f, 487.0f ); //coordinate for building 1
+static Vec2f _Building2 = Vec2f( 200.0f, 487.0f ); //coordinate for building 2
+static Vec2f _Building3 = Vec2f( 510.0f, 487.0f ); //coordinate for building 3
+static Vec2f _Building4 = Vec2f( 660.0f, 487.0f ); //coordinate for building 4
+static Vec2f _Building[4] = {_Building1, _Building2, _Building3, _Building4}; //array of building coordinates
+static Vec2f centerCorrection = Vec2f (40.0f, 40.0f); //correct the coordinates for the building so it points at middle instead of top-left
 
 Missile::Missile()
 {
-     ScoringEngine player;
-     float velocity = player.getVelocity();
-     _health = true;
-    _begLocation = Vec2f (Rand::randFloat(800.0), 0.0f);
-    _location = _begLocation;
-    _buildingNum = Rand::randInt(0,4);
-    _buildingLoc = _Building[_buildingNum] + centerCorrection;
-    _velocity = velocity;
-    _radius = 0;
-    _dirToBuilding = _buildingLoc - _location;
-    _dirToBuilding.safeNormalize();
-    _lifespan = 60;
-    _age = 0;
+     ScoringEngine player;					//ScoringEngine object to extract data
+     float velocity = player.getVelocity();			//velocity from difficulty setting
+     _health = true;						//missile health is true
+    _begLocation = Vec2f (Rand::randFloat(800.0), 0.0f);	//chooses a random beginning location from the top location of the screen
+    _location = _begLocation;					//holds the current location of the missile
+    _buildingNum = Rand::randInt(0,4);				//randomly chooses which building to target
+    _buildingLoc = _Building[_buildingNum] + centerCorrection;	//gets the point at middle of building
+    _velocity = velocity;					//assigns missile velocity from defined ScoringEngine velocity
+    _radius = 0;						//defaults the radius of when it creates the blast
+    _dirToBuilding = _buildingLoc - _location;			//finds the direction to the building
+    _dirToBuilding.safeNormalize();				//  then normalizes the vector
+    _lifespan = 60;						//lifespan of how big the bomb blasts
+    _age = 0;							//sets the age of the missile from when it blasts
 }
 
 void Missile::update()
 {
-     _location += _dirToBuilding * _velocity;
-    if(collisionDetection())
+     _location += _dirToBuilding * _velocity;	//updates location by adding the direction and magnitude 
+    if(collisionDetection())			//if missile hits the building set velocity to 0
     {
         _velocity = 0.0;
     }
-    if (_velocity == 0.0)
+    if (_velocity == 0.0)			//if velocity is 0, increment the age and radius
     {
         _age ++;
         _radius++;
-        if (_age > _lifespan)
+        if (_age > _lifespan)			//until the radius is larger than the lifespan
         {
-            _health = false;
-            if(_structure[_buildingNum] < 3)
+            _health = false;			//kill the missile
+            if(_structure[_buildingNum] < 3)	//increment the building until it is at the lowest level
             {
                 _structure[_buildingNum]++;
-				_eog++;
+		_eog++;
             }
-			else
-				return;
+	    else
+	    	return;
         }
     }
 }
 
 void Missile::draw()
 {
-    if (_velocity != 0.0)
+    if (_velocity != 0.0)				//if missile is moving, draw a line
     {
         gl::drawLine(_begLocation, _location);
     }
-    else
+    else						//if missile is not moving, draw a circle
     {
         gl::drawSolidCircle ( _location, _radius);
     }
@@ -75,7 +73,7 @@ void Missile::draw()
 
 bool Missile::collisionDetection()
 {
-    int left1, left2;
+    int left1, left2;		//create the left, right, top, and bottom of both the building and location
     int right1, right2;
     int top1, top2;
     int bottom1, bottom2;
@@ -89,7 +87,7 @@ bool Missile::collisionDetection()
     bottom1 = static_cast<int>(_location.y);
     bottom2 = static_cast<int>(_buildingLoc.y) + 10;
     
-    if (bottom1 < top2)
+    if (bottom1 < top2) 	//if the location and building intersect, then return false
         return(false);
     if (top1 > bottom2)
         return(false);
