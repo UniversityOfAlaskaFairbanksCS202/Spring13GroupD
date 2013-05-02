9eb33dbe5fd8972e1796c0ecd4cb91447d678068
sjgeorge2 <sjgeorge34@yahoo.com>
2013-05-02 10:33:30 -0700

    comment update

--- TurretMissile.h
+++ TurretMissile.h
@@ -6,23 +6,25 @@
 class TurretMissile
 {
 public:
-    TurretMissile();
-    TurretMissile(ci::Vec2f, ci::Vec2f);
-void update();
-void draw();
-bool collisionDetection();
+// CLASS CONSTRUCTORS //
+    TurretMissile();                        // does nothing //
+    TurretMissile(ci::Vec2f, ci::Vec2f);    // TurretMissile constructor that takes in cursor location and start location. //
+    void update();
+    void draw();
+    bool collisionDetection();              // does what it says, collisionDetection functino for hit checks //
     
-   ci::Vec2f _location;
-ci::Vec2f _cursorLoc;
-    ci::Vec2f _dirToCursor;
-    ci::Vec2f _begLocation;
+// CLASS FUNCTIONS //    
+    ci::Vec2f _location;                    // missile location //
+    ci::Vec2f _cursorLoc;                   // cursor location //
+    ci::Vec2f _dirToCursor;                 // direction to cursor ( _cursorLoc - _location ) //
+    ci::Vec2f _begLocation;                 // start location of missile //
     
-float _velocity;
-int _radius;
-    int _lifespan;
-    int _age;
-    
-bool _health;
+// CLASS VARIABLES //
+    float _velocity;                        // speed of missile //
+    int _radius;                            // radius of missile aka 'hit box' //
+    int _lifespan;                          // slowed growth of explosion at cursor location //
+    int _age;                               // curret missile age, begins at 60 //
+    bool _health;                           // yes = alive, no = EXPLOSION!!! //
     
 };
 
