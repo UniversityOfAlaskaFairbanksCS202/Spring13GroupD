918fabc699bf265361bb2767fe1040ffb564a04e
DrHoodrich <natehelms_8@gmx.com>
2013-05-02 12:11:30 -0700

    Commented

--- Globals.h
+++ Globals.h
@@ -2,7 +2,7 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-extern int _structure[4];
+extern int _structure[4]; //Creates all the external global variables in this functions.
 extern int _eog;
 extern int _choice;
 
