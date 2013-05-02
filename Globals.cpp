0967c4c19c8775dddaf01fc6e2ade81315a90223
DrHoodrich <natehelms_8@gmx.com>
2013-05-02 12:10:45 -0700

    C-c-c-c-commented.

--- Globals.cpp
+++ Globals.cpp
@@ -1,5 +1,5 @@
 #include "globals.h"
 
-int _structure[4]= {0,0,0,0};
-int _eog;
-int _choice;
+int _structure[4]= {0,0,0,0}; //Creates a structure for the buildings.
+int _eog; //Counts how many times you miss a missile(Say that three times fast), and millions of people die in the process. 
+int _choice; //Checks to see if you picked easy or if you decided you wanted to play with the big boys.
