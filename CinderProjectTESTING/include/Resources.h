610a547e26320339802bd7a3c564674833206aaa
DrHoodrich <natehelms_8@gmx.com>
2013-05-01 17:01:14 -0700

    Update Resources.h

--- CinderProjectTESTING/include/Resources.h
+++ CinderProjectTESTING/include/Resources.h
@@ -9,3 +9,6 @@
 #define RES_BUILDING3 CINDER_RESOURCE   (../resources/ , building3.png, 131, IMAGE) //building image after 2 hit
 #define RES_BUILDING4 CINDER_RESOURCE   (../resources/ , building4.png, 132, IMAGE) //building image after 3 hit
 #define RES_FRONTPAGE CINDER_RESOURCE (../resources/ , frontpage.png, 133, IMAGE) //Start Screen
+#define RES_GAMEOVER  CINDER_RESOURCE (../resources/ , GameOver.png, 134 , IMAGE) //Game Over Screen
+//#define RES_ThemeSong CINDER_RESOURCE  (../resources/ , ThemeSong.mp3, 133, MP3) //Test theme song.
+
